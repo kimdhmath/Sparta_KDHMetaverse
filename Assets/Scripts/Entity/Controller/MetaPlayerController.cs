f078a362a6fee059f07fedd3b976e820888a44c1
kimdhmath <kimdhmath0@gmail.com>
2025-02-18 17:30:31 +0900

    ✨<Feat>:"플레이어 움직임 구현"

--- Assets/Scripts/Entity/Controller/MetaPlayerController.cs
+++ Assets/Scripts/Entity/Controller/MetaPlayerController.cs
@@ -15,11 +15,14 @@ public class MetaPlayerController : MonoBehaviour
     protected Vector2 lookDirection = Vector2.zero;
     public Vector2 LookDirection { get { return lookDirection; } }
 
-    Camera mainCamera;
+    private Camera mainCamera;
+
+    [SerializeField] private float speed = 5f;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main; // Initialize mainCamera
     }
 
     private void Update()
@@ -32,14 +35,19 @@ public class MetaPlayerController : MonoBehaviour
         Movement(movementDirection);
     }
 
-    private void Movement(Vector2 dicretion)
+    private void Movement(Vector2 direction)
     {
-        // Movement
+        direction = direction * speed;
+
+        _rigidbody.velocity = direction;
     }
 
     private void Rotate(Vector2 direction)
     {
-        // Rotate
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;
     }
 
     void OnMove(InputValue inputValue)
@@ -52,7 +60,7 @@ public class MetaPlayerController : MonoBehaviour
     {
         Vector2 mousePosition = inputValue.Get<Vector2>();
         Vector2 worldPos = mainCamera.ScreenToWorldPoint(mousePosition);
-        lookDirection = (worldPos - (Vector2)transform.position).normalized;
+        lookDirection = (worldPos - (Vector2)transform.position);
 
         if (lookDirection.magnitude < 0.9f)
         {
