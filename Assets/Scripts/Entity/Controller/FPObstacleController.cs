9c1a8dbaf615b5b004032895fc84f1a0ad80df70
kimdhmath <kimdhmath0@gmail.com>
2025-02-19 10:17:39 +0900

    ✨<Feat>: "장애물 구멍 생성 위치 결정"

--- Assets/Scripts/Entity/Controller/FPObstacleController.cs
+++ Assets/Scripts/Entity/Controller/FPObstacleController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class FPObstacleConroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private readonly float HIGHPOSY = 0.95f;
+    private readonly float LOWPOSY = -0.95f;
 
-    // Update is called once per frame
-    void Update()
+    private readonly float HOLESIZEMIN = 1f;
+    private readonly float HOLESIZEMAX = 3f;
+
+    [SerializeField]private Transform topObject;
+    [SerializeField]private Transform bottomObject;
+
+    [SerializeField]private float widthPadding = 4f;
+
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
     {
-        
+        float holeSize = Random.Range(HOLESIZEMIN, HOLESIZEMAX);
+        float halfHoleSize = holeSize / 2f;
+        topObject.localPosition = new Vector3(0, halfHoleSize);
+        bottomObject.localPosition = new Vector3(0, -halfHoleSize);
+
+        Vector3 placePosition = lastPosition + new Vector3(widthPadding, 0);
+        placePosition.y = Random.Range(LOWPOSY, HIGHPOSY);
+
+        transform.position = placePosition;
+
+        return placePosition;
     }
 }
